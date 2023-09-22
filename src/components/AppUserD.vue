61e95097471d77944bd62543c8bb4628c78cc67b
Karthik K Devadiga <kdevadiga8055@gmail.com>
2023-09-22 06:58:27 +0530

    done all the required changes

--- src/components/AppUserD.vue
+++ src/components/AppUserD.vue
@@ -13,28 +13,28 @@
           href="#"
           class="nav-link__link"
           @click.prevent="current_intrest = 'Profile'"
-          :class="{'link-on-select' : current_intrest === 'Profile'}"
+          :class="{ 'link-on-select': current_intrest === 'Profile' }"
           >Profile</a
         >
         <a
           href="#"
           class="nav-link__link"
           @click.prevent="current_intrest = 'Reporting'"
-          :class="{'link-on-select' : current_intrest === 'Reporting'}"
+          :class="{ 'link-on-select': current_intrest === 'Reporting' }"
           >Reporting</a
         >
         <a
           href="#"
           class="nav-link__link"
           @click.prevent="current_intrest = 'Role'"
-          :class="{'link-on-select' : current_intrest === 'Role'}"
+          :class="{ 'link-on-select': current_intrest === 'Role' }"
           >Role</a
         >
         <a
           href="#"
           class="nav-link__link"
           @click.prevent="current_intrest = 'Performance'"
-          :class="{'link-on-select' : current_intrest === 'Performance'}"
+          :class="{ 'link-on-select': current_intrest === 'Performance' }"
           >Performance</a
         >
       </div>
@@ -70,12 +70,25 @@
       </div> -->
     <!-- </div> -->
     <div class="chat" :class="{ active: chatActive }" @click="toggleChat($event)">
-    Click here to chat
-    <div class="chat__icon-u" ></div>
-    
+      Click here to chat
+      <div class="chat__icon-u">
+        <img
+          width="48"
+          height="48"
+          src="https://img.icons8.com/color/48/communication.png"
+          alt="communication"
+        />
+      </div>
+
       <div class="chat-window" :class="{ growing: chatActive, shrinking: !chatActive }">
         <div>click here to close</div>
-        <div v-for="Cuser in users" :key="user" v-show="Cuser.name != user.name" class="chat-window__profile" @click.prevent="chatWithser(Cuser)">
+        <div
+          v-for="Cuser in users"
+          :key="user"
+          v-show="Cuser.name != user.name"
+          class="chat-window__profile"
+          @click.prevent="chatWithser(Cuser)"
+        >
           <img class="chat-window__img" :src="Cuser.profilepicture" alt="hehe" />
           <p class="chat-window__name">{{ Cuser.name }}</p>
         </div>
@@ -85,19 +98,41 @@
       </div>
     </div>
     <div class="chating-div" :class="{ 'chating-div-display': chatingActive }">
-      <div  class="chat-window__profile chating-profile" >
-          <img class="chat-window__img" :src="currentChatingUser.profilepicture" alt="hehe" />
-          <p class="chat-window__name">{{ currentChatingUser.name }}</p>
+      <div class="chat-window__profile chating-profile">
+        <img class="chat-window__img" :src="currentChatingUser.profilepicture" alt="hehe" />
+        <p class="chat-window__name">{{ currentChatingUser.name }}</p>
+      </div>
+
+      <div class="chat-content">
+        <div class="chat-from" v-for="info in chatInfoFrom" :key="info.time">
+          <span>{{ info.msg }}</span>
         </div>
+
+        <div class="chat-to" v-for="info in chatInfoTo" :key="info.time">
+          <span>{{ info.msg }}</span>
+        </div>
+      </div>
+
+      <form class="input-div" @submit.prevent="onSend">
+        <input v-model="message" />
+        <button type="submit">
+          <img
+            width="35"
+            height="35"
+            src="https://img.icons8.com/color/48/sent--v1.png"
+            alt="sent--v1"
+          />
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'pinia';
-import useModalStore from '../stores/modal';
-import AppUserDetail from './/AppUserDetail.vue';
-import AppUM from './/AppUM.vue';
+import { mapState } from 'pinia'
+import useModalStore from '../stores/modal'
+import AppUserDetail from './/AppUserDetail.vue'
+import AppUM from './/AppUM.vue'
 // import { LMap, LTileLayer, LMarker } from 'vue3-leaflet';
 // import L from 'leaflet';
 // import L from 'vue2-leaflet';
@@ -111,51 +146,61 @@ export default {
       current_intrest: 'Profile',
       chatActive: false,
       chatingActive: false,
-      currentChatingUser:'',
-    };
+      currentChatingUser: '',
+      chatInfoFrom: [
+        { msg: 'Hello', time: '01:25' },
+        { msg: 'Are you there ?', time: '02:35' }
+      ],
+      chatInfoTo: [],
+      message: ''
+    }
   },
   components: {
     AppUserDetail,
-    AppUM,
+    AppUM
   },
   computed: {
-    ...mapState(useModalStore, ['users', 'current_user']),
+    ...mapState(useModalStore, ['users', 'current_user'])
   },
+
   methods: {
     fetchAllDetails() {
-      const user = this.current_user;
-      return this.users[user - 1];
+      const user = this.current_user
+      return this.users[user - 1]
+    },
+
+    onSend() {
+      this.chatInfoTo.push({ msg: this.message, time: '11:01' })
+      this.message = ''
     },
     toggleChat(event) {
-      console.log(event);
-      const target = event.target;
-      console.log(target);
+      console.log(event)
+      const target = event.target
+      console.log(target)
       if (!target.parentNode.classList.contains('chat-window__profile')) {
         // Perform the action only if the class is not present
         // Do something here...
-        this.chatActive = !this.chatActive;
-        if(this.chatingActive){
-          this.chatingActive = false;
+        this.chatActive = !this.chatActive
+        if (this.chatingActive) {
+          this.chatingActive = false
         }
       }
-      
+
       // this.chatingActive = !this.chatingActive
-      
-      
     },
-    chatWithser(user){
+    chatWithser(user) {
       this.currentChatingUser = user
       // this.chatingActive = !this.chatingActive;
-      if(!this.chatingActive){
-        this.chatingActive = true;
+      if (!this.chatingActive) {
+        this.chatingActive = true
       }
     }
   },
   beforeMount() {
     // console.log(this.f);
-    this.user = this.fetchAllDetails();
-    console.log(this.user);
-  },
+    this.user = this.fetchAllDetails()
+    console.log(this.user)
+  }
   // mounted() {
   //   const lat = this.user.address.geo.lat;
   //   const lng = this.user.address.geo.lat;
@@ -172,5 +217,5 @@ export default {
   //     .bindPopup('Guess logitude and latitude are randoms')
   //     .openPopup();
   // },
-};
+}
 </script>
